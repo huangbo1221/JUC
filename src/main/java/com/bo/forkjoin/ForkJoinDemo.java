ef62271a0ea2aac979f6455ba7848e57a7fc6d0f
huangbo1221 <1191958036@qq.com>
2021-10-29 09:15:59 +0800

    初识ForkJoin概念

---
+++ src/main/java/com/bo/forkjoin/ForkJoinDemo.java
@@ -0,0 +1,15 @@
+package com.bo.forkjoin;
+
+/**
+ * @ClassName ForkJoinDemo
+ * @Description 初识ForkJoin
+ * @Author huangbo1221
+ * @Date 2021/10/29 8:46
+ * @Version 1.0
+ */
+
+// 按照ForkJoin概念去理解，大任务怎么拆分成多个小任务？
+// 下面的实例是求和计算的实例
+public class ForkJoinDemo {
+
+}
